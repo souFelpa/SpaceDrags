9d8c3b482d5162e4fa1dd1f1ab4b29295a2a2ab5
hargerri <antonio.harger@hotmail.com>
2015-03-01 00:51:56 -0300

    adcionei a drag

---
+++ space jam 2d/Assets/Scripts/Capture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capture : MonoBehaviour {
+
+	float CaptureTime = 2f;
+	Player currentPlayer = null;
+	List<CaptureProgress> progress;
+	struct CaptureProgress {
+		Player player;
+		float progress;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
