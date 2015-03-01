7a5950c7200278c52e8c9cb1d2f4b2a5d55e4204
hargerri <antonio.harger@hotmail.com>
2015-03-01 03:36:53 -0300

    capture melhorado

--- space jam 2d/Assets/Scripts/Planet.cs
+++ space jam 2d/Assets/Scripts/Planet.cs
@@ -3,19 +3,24 @@ using System.Collections;
 
 public class Planet : MonoBehaviour {
 
+	public bool showDebugRange = false;
 	public float GravityIntensity;
 	public float GravityRange;
 	private GameObject _debugRange;
 	public GameObject prefab;
 	// Use this for initialization
 	void Start () {
-		_debugRange = (GameObject)Instantiate(prefab);
+		if (showDebugRange) {
+						_debugRange = (GameObject)Instantiate (prefab);
+				}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		_debugRange.transform.position = this.transform.position;
-		_debugRange.transform.Translate(0,0,10);
-		_debugRange.transform.localScale = new Vector3(GravityRange*2, GravityRange*2,0);
+		if (showDebugRange) {
+						_debugRange.transform.position = this.transform.position;
+						_debugRange.transform.Translate (0, 0, 10);
+						_debugRange.transform.localScale = new Vector3 (GravityRange * 2, GravityRange * 2, 0);
+				}
 	}
 }
